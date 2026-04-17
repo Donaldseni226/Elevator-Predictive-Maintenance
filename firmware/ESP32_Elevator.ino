3b37fc01006a8e459b8312773e5b48e725586332
Donaldseni226 <ziladonaldalfieri.seni@usmba.ac.ma>
2026-04-17 06:42:26 +0100

    Add main ESP32 code for Elevator-Predictive-Maintenance

---
+++ firmware/ESP32_Elevator.ino
@@ -0,0 +1,23 @@
+// ESP32 code for elevator predictive maintenance
+
+#include <WiFi.h>
+#include <HTTPClient.h>
+
+const char* ssid = "your_SSID";
+const char* password = "your_PASSWORD";
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+
+  Serial.println("Connected to WiFi");
+}
+
+void loop() {
+  // Add your main code here, to run repeatedly:
+}
